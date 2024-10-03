b57e7d7eb908bc040c319fa537ad0081399acdc7
Surbhi-0204 <surbhik531@gmail.com>
2024-10-04 00:20:56 +0530

    Update inPython.py
    Two sum problem of leetcode

--- inPython.py
+++ inPython.py
@@ -1,1 +1,17 @@
+# LEETCODE PROBLEM- TWO SUM (https://leetcode.com/problems/two-sum/submissions/1305905993/)
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        a=[]
+        for j in range (len(nums)-1):
+            for i in range(j+1,len(nums)):
+                if nums[j]+nums[i]==target:
+                    a.append(j)
+                    a.append(i)
+                    break
+        return a
